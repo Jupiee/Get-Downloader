5b3fa837da426c954c1db4ff361fc55b5e2b7bb2
Jupiee <mzkplayz1@gmail.com>
2022-08-27 19:35:46 +0500

    captures file sizes and shows, info storing

--- modules/getcomics.py
+++ modules/getcomics.py
@@ -1,4 +1,5 @@
-import requests, os
+from distutils.log import info
+import requests, os, re
 from bs4 import BeautifulSoup
 
 class Getcomics:
@@ -9,8 +10,10 @@ class Getcomics:
 
         self.temp= []
         self.links= []
+        self.info= {}
         self.currentpage= 0
         self.totalpages= 0
+        self.size_pattern= "Size : \d*\.\d+ |Size : \d+ MB|GB"
 
         try:
             req= requests.get("https://getcomics.info")
@@ -25,11 +28,27 @@ class Getcomics:
         '''Searches for a query/comic in the website and prints out the first page.'''
 
         try:
-            titles= BeautifulSoup(self.changepage(query,1), "html.parser").find_all("h1", class_= "post-title")
-            self.totalpages= int(BeautifulSoup(self.changepage(query,1), "html.parser").find_all("a", class_= "page-numbers")[-1].text)
-            for count,title in enumerate(titles,1):
-                self.temp.append(title.find('a')["href"])
-                print(f"\n{count}. {title.find('a').text}")
+
+            if " " in query:
+                query= query.replace(" ","+")
+            else:
+                pass
+            
+            soupobject= BeautifulSoup(self.changepage(query,1), "html.parser")
+            titles= soupobject.find_all("h1", class_= "post-title")
+            try:
+                self.totalpages= int(BeautifulSoup(self.changepage(query,1), "html.parser").find_all("a", class_= "page-numbers")[-1].text)
+            except:
+                self.totalpages= self.currentpage
+            
+            sizes= self.get_size(soupobject)
+            filtered_titles= self.filter_titles(titles)
+
+            self.update_info(filtered_titles,sizes)
+            self.temp= [title.find('a')["href"] for title in titles]
+
+            for count,i in enumerate(self.info,1):
+                print(f"\n{count}.  {i}     {self.info.get(i)}")
             self.link_substitute()
 
             loop= True
@@ -43,17 +62,29 @@ class Getcomics:
                         print(f"there is no page number {pagenumber}")
                         continue
                     else:
+                        self.temp= []
                         self.links= []
-                        titles= BeautifulSoup(self.changepage(query,int(pagenumber)), "html.parser").find_all("h1", class_= "post-title")
-                        for count,title in enumerate(titles,1):
-                            self.temp.append(title.find('a')["href"])
-                            print(f"\n{count}. {title.find('a').text}")
+                        sizes= []
+                        filtered_titles= []
+                        self.info= {}
+
+                        soupobject= BeautifulSoup(self.changepage(query,int(pagenumber)), "html.parser")
+                        titles= soupobject.find_all("h1", class_= "post-title")
+
+                        sizes= self.get_size(soupobject)
+                        filtered_titles= self.filter_titles(titles)
+
+                        self.update_info(filtered_titles,sizes)
+                        self.temp= [title.find('a')["href"] for title in titles]
+
+                        for count,i in enumerate(self.info,1):
+                            print(f"\n{count}.  {i}     {self.info.get(i)}")
                         self.link_substitute()
                 except:
                     print(f"there is no page number '{pagenumber}' ")
                     continue
         except:
-            print("site is not working")
+            print("site is not working a")
             exit()
             os.system("exit")
 
@@ -85,4 +116,37 @@ class Getcomics:
             self.currentpage= pagenumber
             return req
         except:
-            print("site is not working")
+            print("site is not working b")
+
+    def get_size(self,soupobject):
+
+        sizes_list= []
+
+        sizes= soupobject.find_all("p",class_= "post-excerpt")
+        for size in sizes:
+            x= re.findall(self.size_pattern,size.text)
+            sizes_list.append(x)
+        
+        if len(sizes_list) == 13:
+            sizes_list.pop()
+            return sum(sizes_list,[])
+        else:
+            return sum(sizes_list,[])
+        
+    def filter_titles(self,titles_list):
+
+        titles= []
+
+        for title in titles_list:
+            titles.append(title.find('a').text)
+
+        return(titles)
+
+    def update_info(self,titles,sizes):
+
+        for i in range(12):
+            self.info.update({titles[i]:sizes[i]})
+        
+
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Download Comics from Getcomics.info'
 
 # Setting up
@@ -12,7 +12,7 @@ setup(
     author_email="<faseehshahzad2@gmail.com>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=["bs4","colorama","requests"],    # need to update accordingly
+    install_requires=["bs4","colorama","requests","os","re"],    # need to update accordingly
     keywords=['python', 'comic', 'download', 'downloader', 'comic downloader', 'comics downloader'],
     classifiers=[
         "Development Status :: 1 - Planning",
